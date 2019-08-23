7ba3a050d38193d5fd4eb2c81b2f2e36a7ab5b5a
taransingh27 <taran.singh_cs18@gla.ac.in>
2019-08-23 20:35:07 +0530

    Imported Scanner class and created object(scn) for Scanner class:Gravity.java

---
+++ src/main/Gravity.java
@@ -0,0 +1,14 @@
+
+package main;
+
+import java.util.Scanner;
+
+public class Gravity {
+    public static void main(String[] args) {
+        Scanner scn = new Scanner(System.in);
+
+
+    }
+
+
+}
