a8523ee70a3a03926dabc5b2a47851f9a61a13bf
taransingh27 <taran.singh_cs18@gla.ac.in>
2019-08-23 20:40:58 +0530

    Taking time as input from user, declaration and initialization of required variables:Gravity.java

--- src/main/Gravity.java
+++ src/main/Gravity.java
@@ -6,6 +6,12 @@ import java.util.Scanner;
 public class Gravity {
     public static void main(String[] args) {
         Scanner scn = new Scanner(System.in);
+        System.out.println("Enter value of time");
+        double time = scn.nextDouble();
+        double gravity = 9.8;
+        double distance = 0.0;
+        double speed = 0.0;
+
 
 
     }
